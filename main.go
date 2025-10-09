6e634c5b7a8d3959634f07c1b3b07678ffd4b90a
hartun9 <h4rtun9@gmail.com>
2025-10-09 21:45:54 +0900

    feat: read filepath

--- main.go
+++ main.go
@@ -9,9 +9,14 @@ func main() {
 	fmt.Println(os.Args)
 
 	action := os.Args[1]
+	fileInput := os.Args[2]
+	fileOutput := os.Args[3]
+
 	switch action {
 	case "reverse":
 		fmt.Println("ファイルの中身を逆にします")
+		fmt.Println("fileInput: ", fileInput)
+		fmt.Println("fileOutput: ", fileOutput)
 	default:
 		panic("コマンドが存在しません")
 	}
