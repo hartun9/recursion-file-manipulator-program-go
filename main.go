06f650c48d63a2a0434307bbf9dc4638ec8e222f
hartun9 <h4rtun9@gmail.com>
2025-10-09 22:06:31 +0900

    refactor: rename

--- main.go
+++ main.go
@@ -9,20 +9,20 @@ func main() {
 	fmt.Println(os.Args)
 
 	action := os.Args[1]
-	fileInput := os.Args[2]
-	fileOutput := os.Args[3]
+	inputPath := os.Args[2]
+	outputPath := os.Args[3]
 
 	switch action {
 	case "reverse":
 		// fileInput の内容を反転させ、その結果をfileOutputに書き出す。
 		fmt.Println("ファイルの中身を逆にします")
-		fmt.Println("fileInput: ", fileInput)
-		fmt.Println("fileOutput: ", fileOutput)
-		reverse(fileInput, fileOutput)
+		fmt.Println("inputPath: ", inputPath)
+		fmt.Println("outputPath: ", outputPath)
+		reverse(inputPath, outputPath)
 	default:
 		panic("コマンドが存在しません")
 	}
 }
 
-func reverse(fileInput string, fileOutput string) {
+func reverse(inputPath string, outputPath string) {
 }
