1899d649a9971854325447ec6a2e4905fcbf4437
hartun9 <h4rtun9@gmail.com>
2025-10-11 14:18:28 +0900

    feat: add copy,duplicate-contents,replace-string

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
+	"strconv"
 )
 
 func main() {
@@ -19,10 +20,32 @@ func main() {
 		fmt.Println("inputPath: ", inputPath)
 		fmt.Println("outputPath: ", outputPath)
 		reverse(inputPath, outputPath)
+	case "copy":
+		copy(os.Args[2], os.Args[3])
+	case "duplicate-contents":
+		n, err := strconv.Atoi(os.Args[4])
+		if err != nil {
+			panic(err)
+		}
+		deplicateContents(os.Args[2], n)
+	case "replace-string":
+		replaceString(os.Args[2], os.Args[3], os.Args[4])
 	default:
 		panic("コマンドが存在しません")
 	}
 }
 
 func reverse(inputPath string, outputPath string) {
+}
+
+// copy inputpath にあるファイルのコピーを作成し、outputpath として保存する。
+func copy(inputPath string, outputPath string) {
+}
+
+// deplicateContents inputpath にあるファイルの内容を読み込み、その内容を複製し、複製された内容を inputpath に n 回複製する。
+func deplicateContents(inputPath string, n int) {
+}
+
+// replaceString inputpath にあるファイルの内容から文字列 'needle' を検索し、'needle' の全てを 'newstring' に置き換える。
+func replaceString(inputPath string, needle string, newString string) {
 }
